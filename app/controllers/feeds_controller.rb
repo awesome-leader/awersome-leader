b9c177a9dc818d769888cef86d5cd4b9082d8de4
tmiyamon <miyamototakuya@gmail.com>
2017-06-17 19:40:14 +0900

    add feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,21 @@
 class FeedsController < ApplicationController
   def index
+    @feed = Feed.new
+    @feeds = Feed.includes(:user).order('created_at DESC')
+  end
+
+  def create
+    @feed = Feed.create(feed_params.merge(user: current_user))
+    if @feed.valid?
+      redirect_to feeds_path
+    else
+      @feeds = Feed.includes(:user).order('created_at DESC')
+      render 'index'
+    end
+  end
+
+  private
+  def feed_params
+    params.require(:feed).permit(:body)
   end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,5 @@
 class Feed < ApplicationRecord
   belongs_to :user
+
+  validates :body, presence: true
 end
