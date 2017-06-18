5387199c32c31b780278339e817be8c3d704bfb5
tmiyamon <miyamototakuya@gmail.com>
2017-06-18 12:14:10 +0900

    sort order in reverse order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def ranking
     @users = User.includes(:awesomes)
-    @users = @users.sort_by {|u| u.awesomes.size }
+    @users = @users.sort_by {|u| u.awesomes.size }.reverse
   end
 end
