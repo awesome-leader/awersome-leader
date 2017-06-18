4ef130c23dac492d0816da9e04f7983c1bfdd368
tmiyamon <miyamototakuya@gmail.com>
2017-06-18 16:43:37 +0900

    clean up

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -21,8 +21,8 @@ CarrierWave.configure do |config|
 
 	config.fog_credentials = {
 		:provider               => 'AWS',                             # required
-		:aws_access_key_id      => 'AKIAJTPD2TTDUFLOKAHA',            # required
-		:aws_secret_access_key  => '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',     # required
+		:aws_access_key_id      => '',            # required
+		:aws_secret_access_key  => '',     # required
 		:region                 => 'ap-northeast-1'                        # optional, defaults to 'us-east-1'
 	}
 	config.fog_directory  = 'awesome-leaders'               # required
